21aba8979c3d83411ed773eb79515bbcc34860cf
Thomo <john.thomo96@gmail.com>
2025-01-22 21:13:43 +0300

    End of bookbot project

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     character_count = get_character_count(text)
+    char_list =[]
+    for char, count in character_count.items():
+        if char.isalpha():
+            char_dict = {"char": char, "num": count}
+            char_list.append(char_dict)
+    
+    char_list.sort(reverse=True, key=sort_on)
+
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
-    print(f"{character_count} were found in the document")
+    for char_dict in char_list:
+        print(f"The '{char_dict['char']}' character was found {char_dict['num']} times")
+
+    print("--- End report ---")
 
 
 def get_num_words(text):
@@ -16,6 +28,7 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+
 def get_character_count(text):
     char_counts = {}
     for char in text:
@@ -25,4 +38,12 @@ def get_character_count(text):
         else:
             char_counts[char] = 1
     return(char_counts)
+
+
+def sort_on(dict):
+    return dict["num"]
+        
+   
+
+
 main()
